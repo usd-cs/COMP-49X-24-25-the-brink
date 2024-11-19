a8f9944cf0ea5e6d409cfd480acb7c5d51f8e168
Natalia Orlof-Carson <norlofcarson@sandiego.edu>
2024-11-19 15:24:07 -0800

    Hello World Imoplementation

---
+++ the-brink/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header className= "App-header">
+      <h1>Hello, World!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
