8a539d00ee823d9871db3e87e4a4364f3497b9f2
maeve tierney <maevetierney@sandiego.edu>
2024-12-08 21:53:24 -0800

    there is now a homepage showcasing the 4 different competitions and when you click on the ace competition you are brought to the ace application

--- the-brink/src/App.js
+++ the-brink/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ACEApplicationForm from './AceApply';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Competition from './Competition';
+import AceApply from './AceApply';
+import './Competition.css';
 import './AceApply.css';
 
 function App() {
-  return <ACEApplicationForm/>;
+  return(
+    <Router>
+      <Routes>
+          <Route path="/" element={<Competition />} /> 
+          <Route path ="/ace-apply" element ={<AceApply />}/>
+      </Routes>
+      </Router>
+  )
 }
 
 
