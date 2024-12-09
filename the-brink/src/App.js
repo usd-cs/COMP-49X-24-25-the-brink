454105dffdbe522e36daa9deabd16708e179765a
maeve tierney <maevetierney@sandiego.edu>
2024-12-09 12:48:07 -0800

    Added the login/sign up pages and linked them

--- the-brink/src/App.js
+++ the-brink/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import SignUpPage from './SignUpPage';
+import Login from './Loginpage';
+import './SignUpPage.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Competition from './Competition';
 import AceApply from './AceApply';
@@ -11,10 +14,14 @@ function App() {
       <Routes>
           <Route path="/" element={<Competition />} /> 
           <Route path ="/ace-apply" element ={<AceApply />}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element ={<SignUpPage/>}/>
       </Routes>
       </Router>
   )
 }
+
+
 
 
 export default App;
---
+++ the-brink/src/Loginpage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Loginpage.css';
+import banner from '/Users/maevetierney/Documents/COMP-49X-24-25-the-brink/the-brink/src/PitchSuiteBanner.png';
+
+const Login = () => {
+    const navigate = useNavigate();
+    const handleSignUpClick = () => {
+        navigate('/signup');
+    };
+    return(
+        <div className = 'login-page'>
+            <div className ="banner">
+                <img src={banner} alt="PitchSuite Banner" className="banner-image"/>
+            </div>
+
+
+            <div className="login-box">
+                <h2>Welcome to PitchSuite</h2>
+                <form>
+                    <input type='email' placeholder="Email" className ="input-field" /> 
+                    <input type='password' placeholder= "Password" className="input-field"/>
+                    <button type="submit" className="login-button"> Login</button>
+                </form>
+                <p className ="signup-link">
+                    <span onClick={handleSignUpClick} className='link-text'>Sign Up</span>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
