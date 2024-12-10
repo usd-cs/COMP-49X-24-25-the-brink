70f4d11ceac978d40dc4db0f47462f77d37c7748
Francisco Monarrez <fmonarrezfelix@sandiego.edu>
2024-12-10 01:17:01 -0800

    Completed testing suite for application components

--- the-brink/src/App.js
+++ the-brink/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ProfilePage from './ProfilePage'; // Import ProfilePage component
+import ProfilePage from './ProfilePage';
 import SignUpPage from './SignUpPage';
 import Login from './Loginpage';
 import './SignUpPage.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // Remove BrowserRouter here
 import Competition from './Competition';
 import AceApply from './AceApply';
 import './Competition.css';
@@ -11,15 +11,13 @@ import './AceApply.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Competition />} />
-        <Route path="/ace-apply" element={<AceApply />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/profile" element={<ProfilePage />} /> {/* Added ProfilePage route */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Competition />} />
+      <Route path="/ace-apply" element={<AceApply />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
   );
 }
 
--- the-brink/src/index.js
+++ the-brink/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Ensure App is properly exported in App.js
-import './index.css'; // Optional styling
+import { BrowserRouter, createBrowserRouter } from 'react-router-dom';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Ensure index.html has <div id="root">
+const router = createBrowserRouter(
+  { children: <App /> },
+  { future: { v7_startTransition: true, v7_relativeSplatPath: true } } // Enable future flags
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter router={router}>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
