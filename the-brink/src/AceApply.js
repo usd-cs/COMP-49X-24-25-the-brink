75592211f7fb89f201e2856bea97804762e0c252
maeve tierney <maevetierney@sandiego.edu>
2024-12-09 21:10:27 -0800

    I change go to home to just home and made it blue

--- the-brink/src/AceApply.js
+++ the-brink/src/AceApply.js
@@ -234,7 +234,7 @@ export default function ACEApplicationForm() {
 
 
             </form>
-            <button onClick={goToHomePage} className="home-button">Go to Home</button>
+            <button onClick={goToHomePage} className="home-button">Home</button>
         </div>
         </div>
         </div>
