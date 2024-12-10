27c2b2bc755b75d7d5e8b57616e875534e4a4ad4
Francisco Monarrez <fmonarrezfelix@sandiego.edu>
2024-12-09 17:05:54 -0800

    Add 'Go to Home' button to login and sign-up pages

--- the-brink/src/Loginpage.js
+++ the-brink/src/Loginpage.js
@@ -5,26 +5,32 @@ import banner from './PitchSuiteBanner.png';
 
 const Login = () => {
     const navigate = useNavigate();
-    const handleSignUpClick = () => {
-        navigate('/signup');
-    };
-    return(
-        <div className = 'login-page'>
-            <div className ="banner">
-                <img src={banner} alt="PitchSuite Banner" className="banner-image"/>
-            </div>
 
+    const handleSignUpClick = () => {
+        navigate('/signup'); // Navigate to the sign-up page
+    };
+
+    const handleHomeClick = () => {
+        navigate('/'); // Navigate to the home page
+    };
+
+    return (
+        <div className="login-page">
+            <div className="banner">
+                <img src={banner} alt="PitchSuite Banner" className="banner-image" />
+            </div>
 
             <div className="login-box">
                 <h2>Welcome to PitchSuite</h2>
                 <form>
-                    <input type='email' placeholder="Email" className ="input-field" /> 
-                    <input type='password' placeholder= "Password" className="input-field"/>
-                    <button type="submit" className="login-button"> Login</button>
+                    <input type="email" placeholder="Email" className="input-field" />
+                    <input type="password" placeholder="Password" className="input-field" />
+                    <button type="submit" className="login-button">Login</button>
                 </form>
-                <p className ="signup-link">
-                    <span onClick={handleSignUpClick} className='link-text'>Sign Up</span>
+                <p className="signup-link">
+                    <span onClick={handleSignUpClick} className="link-text">Sign Up</span>
                 </p>
+                <button onClick={handleHomeClick} className="home-button">Go to Home</button>
             </div>
         </div>
     );
--- the-brink/src/SignUpPage.js
+++ the-brink/src/SignUpPage.js
@@ -3,33 +3,37 @@ import { useNavigate } from 'react-router-dom';
 import './SignUpPage.css';
 import banner from './PitchSuiteBanner.png';
 
-const Login = () => {
+const SignUpPage = () => {
     const navigate = useNavigate();
+
     const handleLoginClick = () => {
-        navigate('/login')
-    }
-    
-    return(
-        <div className = 'sign-up-page'>
+        navigate('/login'); // Navigate to the login page
+    };
 
-            <div className ="banner">
-                <img src={banner} alt="PitchSuite Banner" className="banner-image"/>
+    const handleHomeClick = () => {
+        navigate('/'); // Navigate to the home page
+    };
+
+    return (
+        <div className="sign-up-page">
+            <div className="banner">
+                <img src={banner} alt="PitchSuite Banner" className="banner-image" />
             </div>
-
 
             <div className="sign-up-box">
                 <h2>Welcome to PitchSuite</h2>
                 <form>
-                    <input type='email' placeholder="Email" className ="input-field" /> 
-                    <input type='password' placeholder= "Password" className="input-field"/>
-                    <button type="submit" className="sign-up-button"> Sign Up</button>
+                    <input type="email" placeholder="Email" className="input-field" />
+                    <input type="password" placeholder="Password" className="input-field" />
+                    <button type="submit" className="sign-up-button">Sign Up</button>
                 </form>
-                <p className ="login-link">
-                    <span onClick={handleLoginClick} className='link-text'>Login</span>
+                <p className="login-link">
+                    <span onClick={handleLoginClick} className="link-text">Login</span>
                 </p>
+                <button onClick={handleHomeClick} className="home-button">Go to Home</button>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default SignUpPage;
