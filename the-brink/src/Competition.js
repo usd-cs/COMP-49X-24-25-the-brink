9006495edf11bc2b3ebb1ef9aeb2e19a228aa964
Cristian St.Clair <cristianstclair@gmail.com>
2025-02-11 15:14:23 -0800

    refactor: Used 'standard' library to adjust code to fit Javascript Standards

--- the-brink/src/Competition.js
+++ the-brink/src/Competition.js
@@ -1,78 +1,72 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Competition.css';
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Competition.css'
 
 // Corrected relative paths
-import banner from './PitchSuiteBanner.png';
-import aceLogo from './aceCircleLogo.png';
-import usdLogo from './usd-logo.png';
-import socialLogo from './SocialInnovationCircleLogo.png';
-import techLogo from './techCircleLogo.png';
+import banner from './PitchSuiteBanner.png'
+import aceLogo from './aceCircleLogo.png'
+import usdLogo from './usd-logo.png'
+import socialLogo from './SocialInnovationCircleLogo.png'
+import techLogo from './techCircleLogo.png'
 
-const competitionData= [
-    {
-        logo: aceLogo,
-        title: 'Accelerate California Entrepeneurship Pitch Competition',
-        route: '/ace-apply',
-    },
-    {
-        logo: usdLogo,
-        title: 'Fowler Business Concept Challenge',
-    },
-    {
-        logo: socialLogo,
-        title: 'Fowler Global Social Innovation Challenge',
-    },
-    {
-        logo: techLogo,
-        title: 'Torero Entrepeneurship Challenge',
-    },
-];
+const competitionData = [
+  {
+    logo: aceLogo,
+    title: 'Accelerate California Entrepeneurship Pitch Competition',
+    route: '/ace-apply'
+  },
+  {
+    logo: usdLogo,
+    title: 'Fowler Business Concept Challenge'
+  },
+  {
+    logo: socialLogo,
+    title: 'Fowler Global Social Innovation Challenge'
+  },
+  {
+    logo: techLogo,
+    title: 'Torero Entrepeneurship Challenge'
+  }
+]
 
 const Competitions = () => {
+  const navigate = useNavigate()
 
-    const navigate = useNavigate();
+  const handleApplyNowClick = (route) => {
+    navigate(route)
+  }
 
-    const handleApplyNowClick = (route) => {
-        navigate(route);
-    };
+  const handleLoginClick = () => {
+    navigate('/login')
+  }
 
-    const handleLoginClick=() =>{
-        navigate('/login');
-    };
+  return (
+    <div className='competitions'>
 
+      <div className='banner'>
+        <img src={banner} alt='banner' className='banner-image' />
+      </div>
 
-    return(
-        <div className='competitions'>
+      <h2>Competitions</h2>
+      <div className='competitions-grid'>
+        {competitionData.map((competition, index) => (
+          <div className='competition-card' key={index}>
+            <img src={competition.logo} alt='logo' className='competition-logo' />
 
-            <div className="banner">
-                <img src={banner} alt="banner" className='banner-image' />
+            <div className='competition-info'>
+              <h3>{competition.title}</h3>
+              <button
+                className='apply-now-button'
+                onClick={() => handleApplyNowClick(competition.route)}
+              >Apply Now
+              </button>
             </div>
-            
-           
+          </div>
+        ))}
+      </div>
+      <button onClick={handleLoginClick} className='login-button'>Login</button>
+    </div>
+  )
+}
 
-
-            <h2>Competitions</h2>
-            <div className = 'competitions-grid'>
-                {competitionData.map((competition, index) => (
-                    <div className ='competition-card' key={index}>
-                        <img src={competition.logo} alt={'logo'} className="competition-logo" />
-                        
-                        <div className="competition-info">
-                            <h3>{competition.title}</h3>
-                            <button 
-                                className='apply-now-button'
-                                onClick={() => handleApplyNowClick(competition.route)}
-                            >Apply Now</button>
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <button onClick={handleLoginClick} className= 'login-button'>Login</button>
-        </div>
-    );
-};
-
-export default Competitions;
-
-
+export default Competitions
--- the-brink/src/Loginpage.js
+++ the-brink/src/Loginpage.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Loginpage.css';
-import banner from './PitchSuiteBanner.png';
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Loginpage.css'
+import banner from './PitchSuiteBanner.png'
 
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate()
 
-    const handleSignUpClick = () => {
-        navigate('/signup'); // Navigate to the sign-up page
-    };
+  const handleSignUpClick = () => {
+    navigate('/signup') // Navigate to the sign-up page
+  }
 
-    const handleHomeClick = () => {
-        navigate('/'); // Navigate to the home page
-    };
+  const handleHomeClick = () => {
+    navigate('/') // Navigate to the home page
+  }
 
-    return (
-        <div className="login-page">
-            <div className="banner">
-                <img src={banner} alt="PitchSuite Banner" className="banner-image" />
-            </div>
+  return (
+    <div className='login-page'>
+      <div className='banner'>
+        <img src={banner} alt='PitchSuite Banner' className='banner-image' />
+      </div>
 
-            <div className="login-box">
-                <h2>Welcome to PitchSuite</h2>
-                <form>
-                    <input type="email" placeholder="Email" className="input-field" />
-                    <input type="password" placeholder="Password" className="input-field" />
-                    <button type="submit" className="login-button">Login</button>
-                </form>
-                <p className="signup-link">
-                    <span onClick={handleSignUpClick} className="link-text">No Account? Click here to Sign Up</span>
-                </p>
-                <button onClick={handleHomeClick} className="home-button" aria-label="Home">Home</button>
-            </div>
-        </div>
-    );
-};
+      <div className='login-box'>
+        <h2>Welcome to PitchSuite</h2>
+        <form>
+          <input type='email' placeholder='Email' className='input-field' />
+          <input type='password' placeholder='Password' className='input-field' />
+          <button type='submit' className='login-button'>Login</button>
+        </form>
+        <p className='signup-link'>
+          <span onClick={handleSignUpClick} className='link-text'>No Account? Click here to Sign Up</span>
+        </p>
+        <button onClick={handleHomeClick} className='home-button' aria-label='Home'>Home</button>
+      </div>
+    </div>
+  )
+}
 
-export default Login;
+export default Login
