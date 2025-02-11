8f2e2fd6c10c892e719c29408e79fdd537255381
Cristian St.Clair <cristianstclair@gmail.com>
2025-02-11 15:20:19 -0800

    refactor: Installed and ran 'standard' library for JS Linter

--- the-brink/src/App.js
+++ the-brink/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ProfilePage from './ProfilePage';
-import SignUpPage from './SignUpPage';
-import Login from './Loginpage';
-import './SignUpPage.css';
-import { Routes, Route } from 'react-router-dom';
-import Competition from './Competition';
-import AceApply from './AceApply';
-import './Competition.css';
-import './AceApply.css';
+import React from 'react'
+import ProfilePage from './ProfilePage'
+import SignUpPage from './SignUpPage'
+import Login from './Loginpage'
+import './SignUpPage.css'
+import { Routes, Route } from 'react-router-dom'
+import Competition from './Competition'
+import AceApply from './AceApply'
+import './Competition.css'
+import './AceApply.css'
 
-function App() {
+function App () {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Competition />} />
-        <Route path="/ace-apply" element={<AceApply />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path='/' element={<Competition />} />
+        <Route path='/ace-apply' element={<AceApply />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
