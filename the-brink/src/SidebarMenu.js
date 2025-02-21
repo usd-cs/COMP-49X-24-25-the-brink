58fb3fa85d8bd20d74807c4a438bf3d240b0254f
Natalia Orlof-Carson <norlofcarson@sandiego.edu>
2025-02-21 09:01:52 -0800

    Implemented the messages page, along with functional buttons on the sidebar

--- the-brink/src/SidebarMenu.js
+++ the-brink/src/SidebarMenu.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
 import './SidebarMenu.css'; 
+import ProfilePage from './ProfilePage';
+import Messages from './Messages';
 const SidebarMenu = ({ activeTab, setActiveTab }) => {
    // Render the sidebar menu items based on the active tab state
    console.log('rendering sidebar menu');
@@ -31,8 +33,8 @@ const SidebarMenu = ({ activeTab, setActiveTab }) => {
                     <li>Applications</li>
                     <li>Competitions</li>
                     <li>Resources</li>
-                    <li>Profile</li>
-                    <li>Messages</li>
+                    <li><Link to="/profile">Profile</Link></li>
+                    <li><Link to="/messages">Messages</Link></li>
                 </ul>
             </div>
     ); 
@@ -40,19 +42,3 @@ const SidebarMenu = ({ activeTab, setActiveTab }) => {
 
 
 export default SidebarMenu;
-
-
-{/*<div className="sidebar">
-            <div className="sidebar-header">
-                <img src="profile.jpg" alt="Profile" className="sidebar-profile-image" />
-                <h3>John Doe</h3>
-            </div>
-            <ul className="sidebar-menu">
-                {/*<li>
-                        <Link to="/" onClick{() => setActiveTab('profile')} className={activeab === 'profile' ? 'active' : ' '}>Profile</Link>
-                </li>}
-                <li onClick={() => setActiveTab('profile')} className={activeTab === 'profile' ? 'active' : ''}>Profile</li>
-                <li onClick={() => setActiveTab('settings')} className={activeTab === 'settings' ? 'active' : ''}>Settings</li>
-                <li onClick={() => setActiveTab('messages')} className={activeTab === 'messages' ? 'active' : ''}>Messages</li>
-            </ul>
-        </div>*/}
