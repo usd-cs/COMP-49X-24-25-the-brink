6d985f3b695231df4d5c4bef96f1c69b15224032
Cristian St.Clair <71050373+CristianStClairSanDiego@users.noreply.github.com>
2025-04-11 09:13:42 -0700

    Update routing

--- the-brink/src/App.js
+++ the-brink/src/App.js
@@ -1,39 +1,45 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Competition from './Competition'
-import AceApply from './AceApply'
-import Login from './Loginpage'
-import SignUpPage from './SignUpPage'
-import ProfilePage from './ProfilePage'
-import AdminView from './AdminView'
-import DashboardPage from './DashboardPage'
-import ForgotPassword from './ForgotPassword' // Import ForgotPassword component
-import CompetitionDetails from './CompetitionDetails' // Added for the /ace-details route
-import './SignUpPage.css'
-import './Competition.css'
-import './AceApply.css'
-import './AdminView.css'
-import Messages from './Messages'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Competition from './Competition';
+import AceApply from './AceApply';
+import Login from './Loginpage';
+import SignUpPage from './SignUpPage';
+import ProfilePage from './ProfilePage';
+import AdminView from './AdminView';
+import DashboardPage from './DashboardPage';
+import ForgotPassword from './ForgotPassword';
+import ResetPassword from './ResetPassword';
+import CompetitionDetails from './CompetitionDetails';
+import JudgesComps from './JudgesComps';
+import Messages from './Messages';
+import Homepage from './Homepage';
+import AppStatusF from './AppStatusF'; 
+import UserAnnouncements  from './UserAnnouncemnt'
 import OrganizerAnnouncements from './OrganizerAnnouncements'
 
 function App() {
   return (
     <div>
+      {/* Keep Routes Alphabetical by Path! */}
       <Routes>
-        <Route path='/' element={<OrganizerAnnouncements />} />
+        <Route path='/' element={<Homepage />} />
         <Route path='/ace-apply' element={<AceApply />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} /> {/* Forgot password route */}
-        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/ace-details' element={<CompetitionDetails />} />
         <Route path='/admin-view' element={<AdminView />} />
         <Route path='/competitions' element={<Competition />} />
-        <Route path='/ace-details' element={<CompetitionDetails />} />
         <Route path='/dashboard' element={<DashboardPage />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/founder-status' element={<AppStatusF />} /> 
+        <Route path='/judges-comps' element={<JudgesComps />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/messages' element={<Messages />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/user-announcements' element={<UserAnnouncements />} />
+        <Route path='/admin-announcements' element={<OrganizerAnnouncements />} />
       </Routes>
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
